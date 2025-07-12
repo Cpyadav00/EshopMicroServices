1c123f134cac0466cf1fc88dc7cf6a44ad00c616
Chandra Prakash Yadav <hcpyadav2509@gmail.com>
2025-07-12 13:03:27 +0530

    Docker Compose

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//Add services to the container 
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+builder.Services.AddMarten(config =>
+{
+    config.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Configure the HTTP Request pipline 
+
+app.MapCarter();
+
 
 app.Run();
