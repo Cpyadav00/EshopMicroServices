457a049648a28f1261682b6a306866bc27c549aa
Chandra Prakash Yadav <hcpyadav2509@gmail.com>
2025-07-13 00:05:44 +0530

    Get Product By Category

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndPoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.GetProducts;
+
+//public record GetProductRequest();
+public record GetProductsReponse(IEnumerable<Product> Products);
+
+public class GetProductsEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products",async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+            var response = result.Adapt<GetProductsReponse>();
+            return Results.Ok(response);
+
+        })
+            .WithName("GetProducts")
+            .Produces<GetProductsReponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Products");
+    }
+}
